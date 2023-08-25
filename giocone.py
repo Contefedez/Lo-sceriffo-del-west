39f390a34e38cd91db1033c0e99491a0d07a2954
Contefedez <federico.contini@liceobanfi.eu>
2023-08-25 19:04:08 +0200

    punteggio +

--- giocone.py
+++ giocone.py
@@ -2,70 +2,55 @@ import pygame
 from random import randint,choice
 import sys
 
-# class Ostacoli(pygame.sprite.Sprite):
-#     def __init__(self,type):
-#         super().__init__()
-
-#         if type == "mosca":
-#             bersaglio = pygame.image.load("bersaglio.png").convert_alpha()
-#             self.ostacoli = bersaglio
-#         if type == "bandito":
-#             bandito = pygame.image.load("bandito.png").convert_alpha()
-#             self.ostacoli = bandito
-            
-#         self.image = self.ostacoli
-    
-#     def spawn(self):
-        
-
-#     def destroy(self):
-#         if self.rect.x < -100:
-#             self.kill()
-
-
-
-	
-    
-
-
 pygame.init()
 larghezza = 1000
 altezza = 500
 screen = pygame.display.set_mode((larghezza,altezza))
 pygame.display.set_caption("Lo sceriffo del west")
-clock = pygame.time.Clock()
 font = pygame.font.Font(None,50)
 game_active = False
-tempo_inizio = 0
+bianco = (255,255,255)
+
+#tempo
+clock = pygame.time.Clock()
+inizio_round = pygame.time.get_ticks()
+durata_round = 30
+
+#round
 punteggio = 0
-timer = pygame.USEREVENT + 1
-pygame.time.set_timer(timer,1000)
-start = True
+colpi = 0
+max_colpi = 10
+
+#bersagli
+raggio = 30
+bersagli = []
+bersaglio = pygame.image.load("bersaglio.png").convert()
+bandito = pygame.image.load("bandito.png").convert()
 
 #immagini
 sfondo_menu = pygame.image.load("home.png").convert()
 sfondo_menu = pygame.transform.smoothscale(sfondo_menu, (1000,500))
 sfondo_game = pygame.image.load("sfondo.png").convert()
 sfondo_game = pygame.transform.smoothscale(sfondo_game, (1000,500))
-bersaglio = pygame.image.load("bersaglio.png").convert()
 inizio = pygame.image.load("start.png").convert()
 volume = pygame.image.load("volume.png").convert()
 audio = pygame.image.load("audio.png").convert()
-best_score = pygame.image.load("best score.png").convert()
+best_punteggio = pygame.image.load("best punteggio.png").convert()
 dollari = pygame.image.load("dollari.png").convert()
 punteggio = pygame.image.load("punteggio.png").convert()
 canzone = pygame.image.load("canzone.png").convert()
 indietro = pygame.image.load("back.png").convert()
+
 #audio
 
 #classi
 class Button():
-	def __init__(self, pos, text_input, colore = "White" ):
+	def __init__(self, pos, text_input, colore = "bianco" ):
 		self.x_pos = pos[0]
 		self.y_pos = pos[1]
 		self.text_input = text_input
-        self.text = font.render(self.text_input, True, colore)
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+        self.text = self.font.render(self.text_input, True, colore)
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
         self.image = self.text
         self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
         
@@ -79,30 +64,71 @@ class Button():
 titolo_gioco = font.render("LO SCERIFFO DEL WEST", False, (64,64,64))
 rett_titolo = titolo_gioco.get_rect(center = (400,100))
 
+def spawn():
+    z = randint(0,2)
+    if z == 1:
+        x = randint(raggio, larghezza - raggio)
+        y = randint(100, altezza - raggio)
+        bersagli.append((x,y,z))
+    else:
+        z = 2
+        x = randint(raggio, larghezza - raggio)
+        y = randint(100, altezza - raggio)
+        bersagli.append((x,y,z))
+        
+         
 def play():
     while True:
-        PLAY_MOUSE_POS = pygame.mouse.get_pos()
-
-        screen.fill("black")
-
-        PLAY_BACK = Button( pos=(640, 460), text_input="BACK")
-        PLAY_BACK.update(screen)
-
+        tempo_corrente = pygame.time.get_ticks()
+        tempo = (tempo_corrente - inizio_round) / 1000
+        screen.blit(sfondo_game, (0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BACK.input(PLAY_MOUSE_POS):
-                    main_menu()
+            if event.type == pygame.MOUSEBUTTONDOWN and colpi > 0 and tempo < round_duration:
+                if event.button == 1:  # Tasto sinistro del mouse
+                    for target in bersagli:
+                        target_x, target_y = target
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        distance = ((target_x - mouse_x) ** 2 + (target_y - mouse_y) ** 2) ** 0.5
+                        if distance <= raggio:
+                            bersagli.remove(target)
+                            spawn()
+                            punteggio += 1
+                            colpi += 2
 
+                            if colpi > max_colpi:
+                                colpi = max_colpi
+                    
+                    else:  
+                        colpi -= 1
+                    
+                    if colpi == 0:
+                       game_active = False
+        
+        if len(bersagli) < 5 and tempo < durata_round:
+            spawn()
+
+        score_text = font.render(f"Score: {punteggio}", True, bianco)
+        screen.blit(score_text, (125, 10))
+
+        shots_text = font.render(f"Shots: {colpi}", True, bianco)
+        screen.blit(shots_text, (330, 10))
+        
+        while tempo_rimasto > 0:
+            tempo_rimasto = durata_round - tempo
+            time_text = font.render(f"Tempo Rimasto: {tempo_rimasto:.1f}", True, bianco)
+            screen.blit(time_text, (530, 10))
+        game_active = False
+                
         pygame.display.update()
     
 def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        screen.fill("white")
+        screen.fill("bianco")
         
         OPTIONS_BACK = Button( pos=(640, 460), text_input="BACK")
 
