09a149e4fabc771cdbeeb00270af7d5530b0c282
Contefedez <federico.contini@liceobanfi.eu>
2023-08-27 13:19:52 +0200

    gioco finito ma crash nel play

--- giocone.py
+++ giocone.py
@@ -30,10 +30,8 @@ bandito = pygame.image.load("bandito.png").convert()
 rettangolo_bandito = bandito.get_rect(center = (30,30))
 
 #testi
-titolo = font.render("LO SCERIFFO DEL WEST", False, (bianco))
-rettangolo_titolo = titolo.get_rect(center= (150,90))
 canzoni = font.render("CANZONI", False, (bianco))
-rettangolo_canzoni = canzoni.get_rect(center= (150,90))
+rettangolo_canzoni = canzoni.get_rect(center= (350,100))
 
 #immagini
 sfondo_menu = pygame.image.load("home.png").convert()
@@ -41,13 +39,8 @@ sfondo_menu = pygame.transform.smoothscale(sfondo_menu, (1000,500))
 sfondo_game = pygame.image.load("sfondo.png").convert()
 sfondo_game = pygame.transform.smoothscale(sfondo_game, (1000,500))
 inizio = pygame.image.load("start.png").convert()
-volume = pygame.image.load("volume.png").convert()
-audio = pygame.image.load("audio.png").convert()
-best_punteggio = pygame.image.load("best punteggio.png").convert()
-dollari = pygame.image.load("dollari.png").convert()
-punteggio = pygame.image.load("punteggio.png").convert()
-canzone = pygame.image.load("canzone.png").convert()
-indietro = pygame.image.load("back.png").convert()
+
+
 
 #audio
 canzone1 = pygame.mixer.Sound("canzone1.mp3")
@@ -56,24 +49,38 @@ sparo = pygame.mixer.Sound("shot.mp3")
 
 #classi
 class Button():
-	def __init__(self, pos, testo):
-		self.testo = testo
-        self.x_pos = pos[0]
+	def __init__(self, image, pos, font, text_input, base_color, hovering_color):
+		self.image = image
+		self.x_pos = pos[0]
 		self.y_pos = pos[1]
-        self.text = font.render(self.testo, True, bianco)
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-        self.image = self.text
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        
+		self.font = font
+		self.base_color, self.hovering_color = base_color, hovering_color
+		self.text_input = text_input
+		self.text = self.font.render(self.text_input, True, self.base_color)
+		if self.image is None:
+			self.image = self.text
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
 	def update(self, screen):
+		if self.image is not None:
+			screen.blit(self.image, self.rect)
 		screen.blit(self.text, self.text_rect)
+
 	def input(self, position):
 		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
 			return True
 		return False
-#testi
-titolo_gioco = font.render("LO SCERIFFO DEL WEST", False, (64,64,64))
-rett_titolo = titolo_gioco.get_rect(center = (400,100))
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = self.font.render(self.text_input, True, self.hovering_color)
+		else:
+			self.text = self.font.render(self.text_input, True, self.base_color)
+
+
+def get_font(size): 
+    return pygame.font.Font("docktrin.ttf", size)
 
 def spawn():
     z = randint(0,2)
@@ -88,70 +95,73 @@ def spawn():
         bersagli.append((x,y,z))
         
 def play():
-    while True:
-        tempo_corrente = pygame.time.get_ticks()
-        tempo = (tempo_corrente - inizio_round) / 1000
-        screen.blit(sfondo_game, (0,0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and colpi > 0 and tempo < durata_round:
-                if event.button == 1:  
-                    sparo.play()
-                    for bersa in bersagli:
-                        target_x, target_y = bersa[0], bersa[1]
-                        mouse_x, mouse_y = pygame.mouse.get_pos()
-                        distance = ((target_x - mouse_x) ** 2 + (target_y - mouse_y) ** 2) ** 0.5
-                        if distance <= raggio:
-                            bersagli.remove(bersa)
-                            spawn()
-                            punteggio += 1
-                            colpi += 2
-
-                            if colpi > max_colpi:
-                                colpi = max_colpi
-                    
+    screen.blit(sfondo_game, (0,0))
+    tempo_corrente = pygame.time.get_ticks()
+    tempo = (tempo_corrente - inizio_round) / 1000
+    punteggio = 0
+    colpi = 10
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN and colpi > 0 and tempo < durata_round:
+            if event.button == 1:  
+                sparo.play()
+                for bersa in bersagli:
+                    target_x, target_y = bersa[0], bersa[1]
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    distance = ((target_x - mouse_x) ** 2 + (target_y - mouse_y) ** 2) ** 0.5
+                    if distance <= raggio:
+                        bersagli.remove(bersa)
+                        spawn()
+                        punteggio += 1
+                        colpi += 2
                     else:  
                         colpi -= 1
-                    
                     if colpi == 0:
                        main_menu()
-        
-        if len(bersagli) < 5 and tempo < durata_round:
-            spawn()
+    
+    if len(bersagli) < 5 and tempo < durata_round:
+        spawn()
 
-        for bersaglioso in bersagli:
-            if bersaglioso[2] == 1:
-                rettangolo_bersaglio.center = (bersaglioso[0], bersaglioso[1])
-                screen.blit(bersaglio,rettangolo_bersaglio)
-            if bersaglioso[2] == 2:
-                rettangolo_bandito.center = (bersaglioso[0], bersaglioso[1])
-                screen.blit(bandito,(bersaglioso[0],bersaglioso[1]))
+    for bersaglioso in bersagli:
+        if bersaglioso[2] == 1:
+            rettangolo_bersaglio.center = (bersaglioso[0], bersaglioso[1])
+            screen.blit(bersaglio,rettangolo_bersaglio)
+        if bersaglioso[2] == 2:
+            rettangolo_bandito.center = (bersaglioso[0], bersaglioso[1])
+            screen.blit(bandito,rettangolo_bandito)
 
-        score_text = font.render(f"Score: {punteggio}", True, bianco)
-        screen.blit(score_text, (125, 10))
+    score_text = font.render(f"Score: {punteggio}", True, bianco)
+    screen.blit(score_text, (125, 10))
 
-        shots_text = font.render(f"Shots: {colpi}", True, bianco)
-        screen.blit(shots_text, (330, 10))
-        
-        while tempo_rimasto > 0:
-            tempo_rimasto = durata_round - tempo
-            time_text = font.render(f"Tempo Rimasto: {tempo_rimasto:.1f}", True, bianco)
-            screen.blit(time_text, (530, 10))
-        main_menu()
-                
-        pygame.display.update()
+    shots_text = font.render(f"Shots: {colpi}", True, bianco)
+    screen.blit(shots_text, (330, 10))
+    
+    tempo_rimasto = durata_round - tempo
+    while tempo_rimasto > 0:
+        tempo_rimasto = durata_round - tempo
+        time_text = font.render(f"Tempo Rimasto: {tempo_rimasto:.1f}", True, bianco)
+        screen.blit(time_text, (530, 10))
+    main_menu()
+            
+    pygame.display.update()
     
 def options():
     while True:
         mouse_pos = pygame.mouse.get_pos()
         screen.blit(sfondo_menu,(0,0))
-        back_opzioni = Button( pos=(840, 460), text_input="BACK")
+        back_opzioni = Button( image=None, pos=(420, 325), font=get_font(75),text_input="BACK", base_color="White", hovering_color="Green")
         back_opzioni.update(screen)
-        canzone_1 = Button(pos = (500,400), text = "S-1")
-        canzone_2 = Button(pos = (700,400), text = "S-2")
+        canzone_1 = Button( image=None, pos = (270,200), font=get_font(75),text_input = "S-1", base_color="White", hovering_color="Green" )
+        canzone_2 = Button(image=None,pos = (450,200),font=get_font(75), text_input = "S-2", base_color="White", hovering_color="Green")
         screen.blit(canzoni,rettangolo_canzoni)
+        back_opzioni.changeColor(mouse_pos)
+        back_opzioni.update(screen)
+        canzone_1.changeColor(mouse_pos)
+        canzone_1.update(screen)
+        canzone_2.changeColor(mouse_pos)
+        canzone_2.update(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -170,18 +180,24 @@ def options():
 def main_menu():
     while True:
         screen.blit(sfondo_menu, (0, 0))
+        titolo_gioco = get_font(80).render("LO SCERIFFO DEL WEST", False, bianco)
+        rettangolo_titolo = titolo_gioco.get_rect(center = (380,100))
         mouse_pos = pygame.mouse.get_pos()
-        play = Button( pos=(640, 250), text_input="PLAY")
-        options = Button( pos=(640, 400), text_input="OPTIONS")
-        screen.blit(titolo,rettangolo_titolo)
+        play_b = Button( image=None,pos=(340, 200),font=get_font(60), text_input="PLAY",base_color="White", hovering_color="Green")
+        options_b = Button( image=None,pos=(340, 300),font=get_font(60), text_input="OPTIONS",base_color="White", hovering_color="Green")
+        screen.blit(titolo_gioco,rettangolo_titolo)
+        for button in [play_b, options_b]:
+            button.changeColor(mouse_pos)
+            button.update(screen)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if play.input(mouse_pos):
+                if play_b.input(mouse_pos):
                     play()
-                if options.input(mouse_pos):
+                if options_b.input(mouse_pos):
                     options()
                 
 
